74e1eafa501c98d98ada0b063a983fb0dd93f3a8
Carlos Fiebig <cffiebigc@gmail.com>
2017-10-17 12:04:20 -0300

    Add specs for homepage and products

--- spec/integration/search_spec.rb
+++ spec/integration/search_spec.rb
@@ -1,8 +1,19 @@
 require 'spec_helper'
 
-feature 'Visit homepage' do
-  scenario 'should find portal homepage' do
+describe 'Visit homepage' do
+  it 'finds portal homepage' do
     visit '/'
     expect(page).to have_content('Integrarte con un banco ahora')
+  end
+
+  it 'finds portal products' do
+    visit '/productos'
+    expect(page).to have_content('Información del banco')
+    expect(page).to have_content('Identificación')
+    expect(page).to have_content('Créditos de consumo')
+    expect(page).to have_content('Créditos hipotecarios')
+    expect(page).to have_content('Cuentas')
+    expect(page).to have_content('Comercios')
+    expect(page).to have_content('Beneficios')
   end
 end
